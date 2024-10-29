ab11fb4b48793bc61835c396c8cee98a45bc38f7
Harvey <hj_martin@sky.com>
2024-10-29 15:39:07 +0000

    Ignored environ issue

--- assetmanager/settings.py
+++ assetmanager/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import environ
+import environ # type: ignore
 env = environ.Env()
 environ.Env.read_env(env_file=BASE_DIR / ".env")
 
