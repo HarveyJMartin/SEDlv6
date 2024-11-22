197880ae3b42a8d12d8078fdde554be4aaf9b203
Harvey <hj_martin@sky.com>
2024-11-22 11:50:03 +0000

    Login and logout functionality finalised with login required decorator added for home page

--- main/forms.py
+++ main/forms.py
@@ -5,9 +5,11 @@ from django.contrib.auth.models import User
 # User creation form extending the default djanog user creation form and adding requirement for email 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
     
     # This is what is being created by the form, the user.
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["username", "email", "first_name", "last_name", "password1", "password2"]
         
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url="/login")
 def home(request):
     return render(request, 'main/home.html')
 
@@ -12,8 +14,8 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('/home')
     else:
         form = RegisterForm()
-    
+
     return render(request, 'registration/sign_up.html', {"form": form})
