a1533bdb4b14f83ca726d16b61f10025f9812138
Harvey <hj_martin@sky.com>
2024-12-09 14:19:50 +0000

    Created device table and made migrations

--- devices/admin.py
+++ devices/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Device
 
-# Register your models here.
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('device_type', 'manufacturer', 'operating_system', 'model')
+    search_fields = ('manufacturer', 'model')
